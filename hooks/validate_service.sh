ce0ce06f62cfad2f3e5b5bc0f49f6ced5887bc8a
佐藤　悠 <yuu.satoh@jrits.ricoh.co.jp>
2018-10-16 12:23:24 +0900

    Update validate_service.sh

--- hooks/validate_service.sh
+++ hooks/validate_service.sh
@@ -2,7 +2,7 @@
 
 while :; do
  result=$(curl -s http://localhost:3000/)
- if [[ "$result" =~ "Hello DevOps Handson !!change!!" ]]; then
+ if [[ "$result" =~ "Hello DevOps Handson !!change" ]]; then
      exit 0
  else
      sleep 5
